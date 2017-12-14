9652ec35e09ad1c1a6310eeac323eb7b9003689f
Zalak Shah <zalak13@gmail.com>
2017-12-14 00:21:31 -0500

    Assignment Interface

---
+++ Area/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type square struct {
+	sideLength float64
+}
+type triangle struct {
+	height float64
+	base   float64
+}
+type shape interface {
+	getArea() float64
+}
+
+func main() {
+	sq := square{sideLength: 10.5}
+	tr := triangle{height: 5, base: 4}
+	printArea(sq)
+	printArea(tr)
+}
+
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
+
+func (s square) getArea() float64 {
+	return (s.sideLength * s.sideLength)
+}
+
+func (t triangle) getArea() float64 {
+	return (0.5 * t.base * t.height)
+}
